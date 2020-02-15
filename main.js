62be935f3d5387ad1f25acb6975e1ede3c87e63d
ghismo280 <angelo280@hotmail.it>
2020-02-15 01:04:13 +0100

    commit all

--- main.js
+++ main.js
@@ -1,35 +1,3 @@
-/*["login"]
-["id"]
-["node_id"]
-["avatar_url"]
-["gravatar_id"]
-["url"]
-["html_url"]
-["followers_url"]
-["following_url"]
-["gists_url"]
-["starred_url"]
-["subscriptions_url"]
-["organizations_url"]
-["repos_url"]
-["events_url"]
-["received_events_url"]
-["type"]
-["site_admin"]
-["name"]
-["company"]
-["blog"]
-["location"]
-["email"]
-["hireable"]
-["bio"]
-["public_repos"]
-["public_gists"]
-["followers"]
-["following"]
-["created_at"]
-["updated_at"]*/
-
 var myUrl = "https://api.github.com/users/ghismo280"
 
 
@@ -44,45 +12,29 @@ function Get(yourUrl){
 };
 
 let myPromise = Get(myUrl);
-let json_obj = '';
+
+const replaceImage = function (image_url) {
+	document.getElementById("user-image").src = image_url;
+}
+const writeInfo = function (divName, objJsonFormat) {
+	const myDiv = document.getElementById(divName);
+	Object.keys(objJsonFormat).forEach(function(element,index, value) {
+		var node = document.createElement("li");
+		if(objJsonFormat[element]){
+			node.innerHTML = ("<strong>"+element+"</strong>" + ": " + "<span class=\"mylist\">" + objJsonFormat[element] + "</span>");
+		}
+		myDiv.appendChild(node);
+	});
+
+	};
 
 myPromise.then(
 	function(response) {
-		json_obj = JSON.parse(response);
-		console.log(json_obj);
-    	console.log(json_obj["avatar_url"]);
-    	console.log('DOM completamente caricato e analizzato');
-		document.getElementById("user-image").src = json_obj["avatar_url"];
+		let json_obj = JSON.parse(response);
+		replaceImage(json_obj["avatar_url"]);
+		writeInfo("mainroot", json_obj);
 	},
 	function(error) {
 		console.log("Si è verificato l'errore " + error.message);
 	}
 );
-
-
-console.log('hi,man');
-
-console.log('goodbye,man');
-
-
-/*
-function httpGet(url) {
-	return new Promise(function(resolve, reject) {
-		var httpReq = new XMLHttpRequest();
-		httpReq.onreadystatechange = function() {
-			var data;
-			if (httpReq.readyState == 4) {
-				if (httpReq.status == 200) {
-					data = JSON.parse(httpReq.responseText);
-					resolve(data);
-				} else {
-					reject(new Error(httpReq.statusText));
-				}
-			}
-		};
-		httpReq.open("GET", url, true);
-		httpReq.send();
-	});
-}
-
-*/
