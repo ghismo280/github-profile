401ab6a46c785b79a2debb46ad4368aac14a5937
ghismo280 <angelo280@hotmail.it>
2020-02-15 12:45:29 +0100

    last

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const baseUrl = "https://api.github.com/users/";
 const nameIdRoot = "mainroot";
+let keyword = '';
 
 document.getElementById("button-submit").addEventListener("click", function(e) {
-	let myUrl = baseUrl + document.getElementById("text-value").value;
+	keyword = document.getElementById("text-value").value;
+	let myUrl = baseUrl + keyword;
 	const myPromise = Get(myUrl);
+	document.getElementById("text-value").value = '';
 })
+
+document.getElementById("text-value").addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("button-submit").click();
+  }
+});
 
 function Get(yourUrl){
   return new Promise((resolve, reject) => {
@@ -26,12 +36,19 @@ function Get(yourUrl){
 };
 
 const replaceImage = function (image_url) {
+	console.log(image_url);
+	if (image_url === undefined) {
+		image_url = "errorGitHub.png";
+	}
 	document.getElementById("user-image").src = image_url;
 }
 
 const writeInfo = function (divName, objJsonFormat) {
 	const myDiv = document.getElementById(divName);
 	myDiv.innerHTML = "";
+	var nodeSearch = document.createElement("h3");
+	nodeSearch.innerHTML = "<span class=\"my-keyword\">" + keyword + "</span><br>";
+	myDiv.appendChild(nodeSearch);
 	for (key in objJsonFormat) {
 		if(objJsonFormat[key]){
 		var node = document.createElement("li");
@@ -40,3 +57,4 @@ const writeInfo = function (divName, objJsonFormat) {
 		myDiv.appendChild(node);
 	};
 };
+
