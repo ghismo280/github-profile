011e4ba2439aceb804a609299161f0d001c54b56
ghismo280 <angelo280@hotmail.it>
2020-02-15 11:18:12 +0100

    input for search

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-var myUrl = "https://api.github.com/users/ghismo280"
+const baseUrl = "https://api.github.com/users/";
+const nameIdRoot = "mainroot";
 
+document.getElementById("button-submit").addEventListener("click", function(e) {
+	let myUrl = baseUrl + document.getElementById("text-value").value;
+	const myPromise = Get(myUrl);
+})
 
 function Get(yourUrl){
   return new Promise((resolve, reject) => {
@@ -8,33 +13,30 @@ function Get(yourUrl){
     xhr.onload = () => resolve(xhr.responseText);
     xhr.onerror = () => reject(xhr.statusText);
     xhr.send();
-  });
+  }).then(
+		function(response) {
+			const json_obj = JSON.parse(response);
+			replaceImage(json_obj["avatar_url"]);
+			writeInfo(nameIdRoot, json_obj);
+		},
+		function(error) {
+			console.log("Si è verificato l'errore " + error.message);
+		}	
+	);
 };
-
-let myPromise = Get(myUrl);
 
 const replaceImage = function (image_url) {
 	document.getElementById("user-image").src = image_url;
 }
+
 const writeInfo = function (divName, objJsonFormat) {
 	const myDiv = document.getElementById(divName);
-	Object.keys(objJsonFormat).forEach(function(element,index, value) {
+	myDiv.innerHTML = "";
+	for (key in objJsonFormat) {
+		if(objJsonFormat[key]){
 		var node = document.createElement("li");
-		if(objJsonFormat[element]){
-			node.innerHTML = ("<strong>"+element+"</strong>" + ": " + "<span class=\"mylist\">" + objJsonFormat[element] + "</span>");
+			node.innerHTML = ("<strong>" + key + ": " + "</strong>" + "<span class=\"mylist\">" + objJsonFormat[key] + "</span>");
 		}
 		myDiv.appendChild(node);
-	});
-
 	};
-
-myPromise.then(
-	function(response) {
-		let json_obj = JSON.parse(response);
-		replaceImage(json_obj["avatar_url"]);
-		writeInfo("mainroot", json_obj);
-	},
-	function(error) {
-		console.log("Si è verificato l'errore " + error.message);
-	}
-);
+};
